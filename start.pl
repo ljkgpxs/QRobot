ceaa116b7cbc680d728f89d54797943c3942dca6
ljkgpxs <uasoft@yeah.net>
2015-08-28 15:43:17 +0800

    更换QQ号和密码的键入，方式为从键盘键入

--- start.pl
+++ start.pl
@@ -4,11 +4,15 @@ use 5.010;
 use Mojo::Webqq;
 use Mojo::Util qw(md5_sum);
 
-# 在此键入QQ号和密码
-my $qq = 123456789;
-my $pwd = md5_sum('password');
-my $client = Mojo::Webqq->new(ua_debug=>0);
-$client->login(qq => $qq, pwd => $pwd);
+sub create_account {
+	open(AFILE, ">./account.txt");
+	print "请输入QQ号： ";
+	chomp($qq = <STDIN>);
+	print "请输入密码： ";
+	chomp($pwd = <STDIN>);
+	$pwd = md5_sum($pwd);
+	print AFILE $qq . "\n" . $pwd;
+}
 
 sub read_from_file
 {
@@ -28,29 +32,46 @@ sub passmsg
 	&read_from_file($_[0]);
 }
 
+my $qq = undef;
+my $pwd = undef;
 
+if(-e "./account.txt") {
+	open (AFILE, "<./account.txt");
+	chomp($qq = <AFILE>);
+	chomp($pwd = <AFILE>);
+	close(AFILE);
+	if($qq && $pwd) {
+	} else {
+		&create_account;
+	}
+} else {
+	&create_account;
+}
+
+say "debug  ".$qq."\n".$pwd;
+my $client = Mojo::Webqq->new(ua_debug=>0);
+$client->login(qq => $qq, pwd => $pwd);
 $client->load("ShowMsg");
 $client->on(receive_message=>sub{
-    my ($client,$msg) = @_;
-   
-    if($msg->type eq 'message')
-    {
-	system("./core/start.sh", $msg->type, $msg->sender->qq, $msg->content);
-    	my @reply_content = &passmsg($msg->sender->qq);
-#	say "Debug   " . $msg->sender->qq;
-	if(@reply_content) {
-		my $reply_text;
-		foreach (@reply_content) {
-			$reply_text .= $_;
+	my ($client,$msg) = @_;
+	if($msg->type eq 'message') {
+		system("./core/start.sh", $msg->type, $msg->sender->qq, $msg->content);
+		my @reply_content = &passmsg($msg->sender->qq);
+#		say "Debug   " . $msg->sender->qq;
+		if(@reply_content) {
+			my $reply_text;
+			foreach (@reply_content) {
+				$reply_text .= $_;
+			}
+			$client->reply_message($msg, $reply_text);
+		} else {
+			say "";
+			say "System message:No Reply";
 		}
-		$client->reply_message($msg, $reply_text);
-	} else {
-		say "";
-		say "System message:No Reply";
 	}
-    }
-#    $client->reply_message($msg,$msg->{content});
+	$client->reply_message($msg,$msg->{content});
 });
+
 $client->run;
 system("rm /tmp/*.reply");
 
